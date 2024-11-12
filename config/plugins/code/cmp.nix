5711a3bd9d2bc73eb1cc751d36b7d798b6f0a72b
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-12 18:27:43 -0500

    Switch to cmp; add other cmp plugins

--- config/plugins/code/cmp.nix
+++ config/plugins/code/cmp.nix
@@ -1,6 +1,29 @@
 {
-  plugins.blink-cmp = {
-    enable = true;
+  plugins = {
+    cmp = {
+      enable = true;
+    };
+
+    cmp-nvim-lsp = {
+      enable = true;
+    };
+    
+    cmp-buffer = {
+      enable = true;
+    };
+    
+    cmp-path = {
+      enable = true;
+    };
+    
+    cmp_luasnip = {
+      enable = true;
+    };
+    
+    cmp-cmdline = {
+      enable = true;
+    };    
   };
 }
+
 
