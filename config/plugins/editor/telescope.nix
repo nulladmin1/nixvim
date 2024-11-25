772dc97ae5f4ab2e75dda05e7a288118aa256a40
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-24 22:33:10 -0500

    Add frecency and keymaps for telescope

--- config/plugins/editor/telescope.nix
+++ config/plugins/editor/telescope.nix
@@ -1,5 +1,25 @@
 {
   plugins.telescope = {
     enable = true;
+
+    extensions = {
+      frecency.enable = true;
+    };
+
+    keymaps = {
+      "<leader>f" = {
+        action = "find_files";
+        options = {
+          desc = "Telescope File Finder";
+        };
+      };
+      "<leader>g" = {
+        action = "git_files";
+        options = {
+          desc = "Telescope Git File Finder";
+        };
+      };
+      "\\" = "live_grep";
+    };
   };
 }
