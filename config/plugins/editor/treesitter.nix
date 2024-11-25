af2b6378303ddcc84822ff27da6ec999bc144115
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-24 22:33:34 -0500

    Add Treesiter with all supported

--- config/plugins/editor/treesitter.nix
+++ config/plugins/editor/treesitter.nix
@@ -1,3 +1,47 @@
 {
+  plugins.treesitter = {
+    enable = true;
 
+    settings = {
+      indent.enable = true;
+      highlight.enable = true;
+      autopairs.enable = true;
+      folding.enable = true;
+
+      auto_install = true;
+
+      ensure_installed = [
+        "all"
+        #  "bash"
+        #  "c"
+        #  "diff"
+        #  "html"
+        #  "javascript"
+        #  "java"
+        #  "jsdoc"
+        #  "json"
+        #  "jsonc"
+        #  "lua"
+        #  "luadoc"
+        #  "luap"
+        #  "nix"
+        #  "markdown"
+        #  "markdown_inline"
+        #  "printf"
+        #  "python"
+        #  "query"
+        #  "regex"
+        #  "rust"
+        #  "toml"
+        #  "tsx"
+        #  "typescript"
+        #  "tsx"
+        #  "vim"
+        #  "vimdoc"
+        #  "xml"
+        #  "toml"
+        #  "yaml"
+      ];
+    };
+  };
 }
