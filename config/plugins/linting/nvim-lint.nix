7e67477e5c2ecf0ba5f1535451ec3aff584dfcac
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-25 18:18:47 -0500

    Disable lintersByFiletype for now

--- config/plugins/linting/nvim-lint.nix
+++ config/plugins/linting/nvim-lint.nix
@@ -4,21 +4,21 @@
       enable = true;
 
       lintersByFt = {
-        bash = [ "shellcheck" ];
-        json = [ "jsonlint" ];
-        markdown = [ "vale" ];
-        rst = [ "vale" ];
-        text = [ "vale" ];
-        c = [ "cpplint" ];
-        cpp = [ "cpplint" ];
-        nix = [ "nix" "statix" "deadnix" ];
-        go = [ "golangci-lint" ];
-        lua = [ "selene" ];
-        python = [ "ruff" ];
-        javascript = [ "eslint_d" ];
-        javascriptreact = [ "eslint_d" ];
-        java = [ "checkstyle" ];
-        rust = [ "clippy" ];
+        #    bash = [ "shellcheck" ];
+        #    json = [ "jsonlint" ];
+        #    markdown = [ "vale" ];
+        #    rst = [ "vale" ];
+        #    text = [ "vale" ];
+        #    c = [ "cpplint" ];
+        #    cpp = [ "cpplint" ];
+        #    go = [ "golangci-lint" ];
+        #    lua = [ "selene" ];
+        #    python = [ "ruff" ];
+        #    javascript = [ "eslint_d" ];
+        #    javascriptreact = [ "eslint_d" ];
+        #    java = [ "checkstyle" ];
+        #    rust = [ "clippy" ];
+        #    cmake = [ "cmakelint" ];
       };
     };
   };
