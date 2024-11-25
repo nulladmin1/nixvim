19435773d30484ca4b270fbcb4983aff8bbf2341
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-24 22:57:08 -0500

    Add keymap space+e for neotree

--- config/plugins/ui/neotree.nix
+++ config/plugins/ui/neotree.nix
@@ -2,4 +2,17 @@
   plugins.neo-tree = {
     enable = true;
   };
+
+  keymaps = [
+    {
+      mode = [
+        "n"
+      ];
+      key = "<leader>e";
+      action = "<cmd>Neotree toggle<cr>";
+      options = {
+        desc = "Open or Close Neotree";
+      };
+    }
+  ];
 }
