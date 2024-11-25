3488ecbc61ccf16d5d1dd0c0a38fea44fa25c58a
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-24 21:45:09 -0500

    Add Alejandra for formatting

--- flake.nix
+++ flake.nix
@@ -9,18 +9,23 @@
     };
   };
 
-  outputs =
-    { nixvim, nixpkgs, ... }: let
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forEachSystem = nixpkgs.lib.genAttrs systems;
-      pkgs = forEachSystem (system: import nixpkgs {inherit system;});
-      nixvimPkgs = forEachSystem (system: nixvim.legacyPackages.${system});
-    in {
-        packages = forEachSystem (system: {
-          default = nixvimPkgs.${system}.makeNixvimWithModule {
-            pkgs = pkgs.${system};
-            module = ./config;
-          };
-        });
-    };
+  outputs = {
+    nixvim,
+    nixpkgs,
+    ...
+  }: let
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgs = forEachSystem (system: import nixpkgs {inherit system;});
+    nixvimPkgs = forEachSystem (system: nixvim.legacyPackages.${system});
+  in {
+    packages = forEachSystem (system: {
+      default = nixvimPkgs.${system}.makeNixvimWithModule {
+        pkgs = pkgs.${system};
+        module = ./config;
+      };
+    });
+
+    formatter = forEachSystem (system: pkgs.${system}.alejandra);
+  };
 }
