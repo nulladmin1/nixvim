30a8f385ead7fdaa4ab0367a059c528c8409ff62
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-25 19:05:41 -0500

    Readd linters with cmd to pkgs

--- config/plugins/linting/nvim-lint.nix
+++ config/plugins/linting/nvim-lint.nix
@@ -1,24 +1,55 @@
-{
+{pkgs, ...}: {
   plugins = {
     lint = {
       enable = true;
 
+      linters = {
+        clippy = {
+          cmd = "${pkgs.clippy}/bin/clippy";
+        };
+        cpplint = {
+          cmd = "${pkgs.cpplint}/bin/cppint";
+        };
+        vale = {
+          cmd = "${pkgs.vale}/bin/vale";
+        };
+        golangcilint = {
+          cmd = "${pkgs.golangci-lint}/bin/golangci-lint";
+        };
+        selene = {
+          cmd = "${pkgs.selene}/bin/selene";
+        };
+        ruff = {
+          cmd = "${pkgs.ruff}/bin/ruff";
+        };
+        eslint_d = {
+          cmd = "${pkgs.eslint_d}/bin/eslint_d";
+        };
+        checkstyle = {
+          cmd = "${pkgs.checkstyle}/bin/checkstyle";
+        };
+        statix = {
+          cmd = "${pkgs.statix}/bin/statix";
+        };
+      };
+
       lintersByFt = {
-        #    bash = [ "shellcheck" ];
-        #    json = [ "jsonlint" ];
-        #    markdown = [ "vale" ];
-        #    rst = [ "vale" ];
-        #    text = [ "vale" ];
-        #    c = [ "cpplint" ];
-        #    cpp = [ "cpplint" ];
-        #    go = [ "golangci-lint" ];
-        #    lua = [ "selene" ];
-        #    python = [ "ruff" ];
-        #    javascript = [ "eslint_d" ];
-        #    javascriptreact = [ "eslint_d" ];
-        #    java = [ "checkstyle" ];
-        #    rust = [ "clippy" ];
-        #    cmake = [ "cmakelint" ];
+        bash = ["shellcheck"];
+        json = ["jsonlint"];
+        markdown = ["vale"];
+        rst = ["vale"];
+        text = ["vale"];
+        c = ["cpplint"];
+        cpp = ["cpplint"];
+        go = ["golangcilint"];
+        lua = ["selene"];
+        python = ["ruff"];
+        javascript = ["eslint_d"];
+        javascriptreact = ["eslint_d"];
+        java = ["checkstyle"];
+        rust = ["clippy"];
+        cmake = ["cmakelint"];
+        nix = ["statix"];
       };
     };
   };
