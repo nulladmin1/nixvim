ea5ee51e5a1ece55ddefb5053f92135aae581d03
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-10 21:59:47 -0500

    Add documentation for opts

--- config/opts.nix
+++ config/opts.nix
@@ -1,9 +1,19 @@
 {...}: {
+  # Editor options for Nixvim
   opts = {
+    # Number of spaces that a <Tab> in the file counts for
     tabstop = 2;
+
+    # Number of spaces to use for each step of (auto)indent
     shiftwidth = 2;
+
+    # Number of spaces that a <Tab> counts for while performing editing operations
     softtabstop = 2;
+
+    # Use spaces instead of tabs
     expandtab = true;
+
+    # Copy indent from current line when starting a new line
     autoindent = true;
   };
 }
