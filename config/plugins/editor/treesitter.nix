8dbc3cec58f19b89f146a089e1bdc775ebe4dec9
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-11 23:30:14 -0500

    Add docs for nvim-treesitter

--- config/plugins/editor/treesitter.nix
+++ config/plugins/editor/treesitter.nix
@@ -1,15 +1,28 @@
 {
+  # Enable the nvim-treesitter plugin - implementation of Treesitter for Neovim
+  # See source: https://github.com/nvim-treesitter/nvim-treesitter, options: https://nix-community.github.io/nixvim/search/?query=plugins.treesitter.&option_scope=0&option=plugins.treesitter.enable
   plugins.treesitter = {
     enable = true;
 
+    # Settings for Nvim-Treesitter
     settings = {
+      # Enable Treesitter:
+      # indent
       indent.enable = true;
+
+      # highlighting
       highlight.enable = true;
+
+      # autopairs
       autopairs.enable = true;
+
+      # folding
       folding.enable = true;
 
+      # Enable auto_install for Treesitter languages
       auto_install = true;
 
+      # Install all Treesitter languages
       ensure_installed = "all"; # [
       # "all"
       #  "bash"
