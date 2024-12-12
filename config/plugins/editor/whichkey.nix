81921be9b077104ed0b904e64c711d754ea9b788
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-11 23:32:08 -0500

    Add docs for whichkey

--- config/plugins/editor/whichkey.nix
+++ config/plugins/editor/whichkey.nix
@@ -1,4 +1,6 @@
 {
+  # Enable which-key - a popup that shows keybindings for the command you are currently typing
+  # See source: https://github.com/folke/which-key.nvim, options: https://nix-community.github.io/nixvim/search/?query=plugins.which-key&option_scope=0&option=plugins.which-key.enable
   plugins = {
     which-key = {
       enable = true;
