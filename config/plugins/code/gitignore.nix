574da0c4ea229c79b4feee9ce3b450c4c5e2dc8a
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-11 21:53:12 -0500

    Add docs for gitignore.nvim

--- config/plugins/code/gitignore.nix
+++ config/plugins/code/gitignore.nix
@@ -1,6 +1,10 @@
 {
+  # Enable gitignore.nvim
+  # See source: https://github.com/wintermute-cell/gitignore.nvim, options: https://nix-community.github.io/nixvim/search/?query=plugins.gitignore&option_scope=0&option=plugins.gitignore.enable
   plugins.gitignore = {
     enable = true;
+
+    # Set keybinding <leader>gi to generate gitignore
     keymap = {
       key = "<leader>gi";
       mode = ["n"];
