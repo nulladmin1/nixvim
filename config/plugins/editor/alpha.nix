5bed652e69d19da54b2ca41bdd0a8f2189ebbbcc
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-11 22:14:16 -0500

    Add docs for Alpha.nvim

--- config/plugins/editor/alpha.nix
+++ config/plugins/editor/alpha.nix
@@ -1,6 +1,10 @@
 {
+  # Enable alpha startup screen
+  # See source: https://github.com/goolord/alpha-nvim, options: https://nix-community.github.io/nixvim/search/?query=plugins.alpha&option_scope=0&option=plugins.alpha.enable
   plugins.alpha = {
     enable = true;
+
+    # Set the theme to dashboard
     theme = "dashboard";
   };
 }
