83400395e4e8a91a3f449ef213ec3e1a11137f75
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-11 21:53:05 -0500

    Add docs for copilot-cmp

--- config/plugins/code/copilot-cmp.nix
+++ config/plugins/code/copilot-cmp.nix
@@ -1,11 +1,18 @@
 {
+  # Enable GitHub Copilot (free using GitHub Education)
   plugins = {
+    # Enable Copilot using copilot.lua (replacement for copilot.vim)
+    # See source: https://github.com/zbirenbaum/copilot.lua
     copilot-lua = {
       enable = true;
+
+      # These 2 needed to be disabled to use with copilot-cmp
       suggestion.enabled = false;
       panel.enabled = false;
     };
 
+    # CMP Source for Copilot
+    # See source: https://github.com/zbirenbaum/copilot-cmp
     copilot-cmp = {
       enable = true;
     };
