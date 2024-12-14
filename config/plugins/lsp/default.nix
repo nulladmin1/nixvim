79ee17983dcf17a74a0e6647bd36b26c6c018577
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-14 17:34:23 -0500

    Add lualines

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./lsp.nix
+    ./lsp_lines.nix
   ];
 }
---
+++ config/plugins/lsp/lsp_lines.nix
@@ -0,0 +1,7 @@
+{
+  # Enable lsp_lines - show diagnostics in a better way
+  # See source: https://git.sr.ht/~whynothugo/lsp_lines.nvim, options: https://nix-community.github.io/nixvim/search/?query=lsp-lines&option_scope=0&option=plugins.lsp-lines.enable
+  plugins.lsp-lines = {
+    enable = true;
+  };
+}
