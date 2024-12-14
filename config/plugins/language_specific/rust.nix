2b396901787ccfe88ea859d817358d8169091fff
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-14 01:21:15 -0500

    Add docs for Rust.nix

--- config/plugins/language_specific/rust.nix
+++ config/plugins/language_specific/rust.nix
@@ -1,22 +1,31 @@
 {
+  # Unlike all other languages, using rustaceanvim to add a multitude of features for Rust
+  # See source: https://github.com/mrcjkb/rustaceanvim, options: https://nix-community.github.io/nixvim/search/?query=plugins.rustaceanvim&option_scope=0&option=plugins.rustaceanvim.enable
   plugins.rustaceanvim = {
     enable = true;
 
     settings = {
+      # Autoload the configuration for DAP
       dap.autoload_configurations = true;
 
+      # Server settings
       server = {
         default_settings = {
+          # Rust Analyzer settings
           rust-analyzer = {
+            # Enable all Cargo features
             cargo.features = "all";
 
+            # Enable style lints
             diagnostics.styleLints.enable = true;
 
+            # Use clippy to check for errors
             check = {
               command = "clippy";
               features = "all";
             };
 
+            # Ignored by rust-analyzer
             files.excludeDirs = [
               ".cargo"
               ".direnv"
@@ -25,6 +34,7 @@
               "target"
             ];
 
+            # Enlay hints settings
             inlayHints = {
               bindingModeHints.enable = true;
               closureStyle = "rust_analyzer";
@@ -36,6 +46,7 @@
               rangeExclusiveHints.enable = true;
             };
 
+            # Discover the source for Rustc
             rustc.source = "discover";
           };
         };
